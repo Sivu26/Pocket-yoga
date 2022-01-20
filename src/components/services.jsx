29c70a88eaf60eaa123212245c2d24f7a20cf625
Sivu26 <98076152+Sivu26@users.noreply.github.com>
2022-01-20 12:25:11 +0530

    Add files via upload

---
+++ src/components/services.jsx
@@ -0,0 +1,92 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Motivational Stories</h2>
+          <p>
+            <b>1.Laziness won’t get you anywhere</b><br></br>
+            <br></br>
+
+            “In ancient times, a king had his men place a boulder on a roadway. He then hid in the bushes, and watched to see if anyone would move the boulder out of the way. Some of the king`s wealthiest merchants and courtiers passed by and simply walked around it.
+
+Many people blamed the King for not keeping the roads clear, but none of them did anything about getting the stone removed.
+
+One day, a peasant came along carrying vegetables. Upon approaching the boulder, the peasant laid down his burden and tried to push the stone out of the way. After much pushing and straining, he finally managed.
+
+After the peasant went back to pick up his vegetables, he noticed a purse lying in the road where the boulder had been. The purse contained many gold coins and note from the King explain that the gold was for the person who removed the boulder from the road.”
+          </p><br></br>
+          <h5>2.Stop wasting your time complaining</h5> <br></br>
+            <br></br>
+          <p>
+          “People visit a wise man complaining about the same problems over and over again. One day, he decided to tell them a joke and they all roared with laughter.
+
+After a few minutes, he told them the same joke and only a few of them smiled.
+
+Then he told the same joke for a third time, but no one laughed or smiled anymore.
+
+The wise man smiled and said: ‘You can’t laugh at the same joke over and over. So why are you always crying about the same problem?'”
+          </p><br></br>
+         <h5>3.Struggling will make you stronger</h5><br></br>
+            <br></br>
+          <p>
+          “Once upon a time, a man found a butterfly that was starting to hatch from its cocoon. He sat down and watched the butterfly for hours as it struggled to force itself through a tiny hole. Then, it suddenly stopped making progress and looked like it was stuck.
+
+Therefore, the man decided to help the butterfly out. He took a pair of scissors and cut off the remaining bit of the cocoon. The butterfly then emerged easily, although it had a swollen body and small, shriveled wings.
+
+The man thought nothing of it, and he sat there waiting for the wings to enlarge to support the butterfly. However, that never happened. The butterfly spent the rest of its life unable to fly, crawling around with small wings and a swollen body.
+
+Despite the man’s kind heart, he didn’t understand that the restricting cocoon and the struggle needed by the butterfly to get itself through the small hole were God’s way of forcing fluid from the body of the butterfly into its wings to prepare itself for flying once it was free.”
+          </p><br></br>
+         <h5>4.Your reaction matters more than what happens to you</h5><br></br>
+            <br></br>
+          <p>
+          “Once upon a time a daughter complained to her father that her life was miserable and that she didn’t know how she was going to make it. She was tired of fighting and struggling all the time. It seemed just as one problem was solved, another one soon followed.
+
+Her father, a chef, took her to the kitchen. He filled three pots with water and placed each on a high fire. Once the three pots began to boil, he placed potatoes in one pot, eggs in the second pot, and ground coffee beans in the third pot.
+
+He then let them sit and boil, without saying a word to his daughter. The daughter, moaned and impatiently waited, wondering what he was doing.
+
+After twenty minutes he turned off the burners. He took the potatoes out of the pot and placed them in a bowl. He pulled the boiled eggs out and placed them in a bowl.
+
+He then ladled the coffee out and placed it in a cup. Turning to her he asked. ‘Daughter, what do you see?’
+
+‘Potatoes, eggs, and coffee,’ she hastily replied.
+
+‘Look closer,’ he said, ‘and touch the potatoes.’ She did and noted that they were soft. He then asked her to take an egg and break it. After pulling off the shell, she observed the hard-boiled egg. Finally, he asked her to sip the coffee. Its rich aroma brought a smile to her face.
+
+‘Father, what does this mean?’ she asked.
+
+He then explained that the potatoes, the eggs and coffee beans had each faced the same adversity– the boiling water.
+
+However, each one reacted differently.
+
+The potato went in strong, hard, and unrelenting, but in boiling water, it became soft and weak.
+
+The egg was fragile, with the thin outer shell protecting its liquid interior until it was put in the boiling water. Then the inside of the egg became hard.
+
+However, the ground coffee beans were unique. After they were exposed to the boiling water, they changed the water and created something new.
+
+‘Which are you,’ he asked his daughter. ‘When adversity knocks on your door, how do you respond? Are you a potato, an egg, or a coffee bean?’
+          </p><br></br>
+         <h5>5.Ignore the haters</h5><br></br>
+            <br></br>
+          <p>
+          “A group of frogs were traveling through the forest when two of them fell into a deep pit. When the other frogs saw how deep the pit was, they told the two frogs that there was no hope left for them.
+
+However, the two frogs ignored their comrades and proceeded to try to jump out of the pit. However, despite their efforts, the group of frogs at the top of the pit were still saying that they should just give up as they’d never make it out.
+
+Eventually, one of the frogs took heed of what the others were saying and he gave up, jumping even deeper to his death. The other frog continued to jump as hard as he could. Once again, the group of frogs yelled at him to stop the pain and to just die.
+
+He ignored them, and jumped even harder and finally made it out. When he got out, the other frogs said, ‘Did you not hear us?’
+
+The frog explained to them that he was deaf, and that he thought they were encouraging him the entire time.”
+          </p><br></br>
+        </div>
+        </div>
+      </div>
+   
+  )
+}
+
+export default Services
